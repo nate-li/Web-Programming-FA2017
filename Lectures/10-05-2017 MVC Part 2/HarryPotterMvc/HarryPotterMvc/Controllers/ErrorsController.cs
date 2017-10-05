aa9d3e5964e6a9adb40968d9eaa66d0e48b26b73
Steven Yackel <spazard1@gmail.com>
2017-10-05 16:04:11 -0500

    Add lecture 10-5-2017

---
+++ Lectures/10-05-2017 MVC Part 2/HarryPotterMvc/HarryPotterMvc/Controllers/ErrorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotterMvc.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [Route("/Error")]
+        public void Index()
+        {
+            var feature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = feature?.Error;
+          
+            Console.Write(exception);
+        }
+    }
+}
---
+++ Lectures/10-05-2017 MVC Part 2/HarryPotterMvc/HarryPotterMvc/Controllers/SpellsController.cs
@@ -0,0 +1,57 @@
+﻿using HarryPotterMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotterMvc.Controllers
+{
+    public class SpellsController : Controller
+    {
+        
+        public static AllSpellsModel spells = new AllSpellsModel();
+
+
+        public IActionResult Index()
+        {
+            return View(spells);
+        }
+
+        public IActionResult ViewSpell(string id)
+        {
+            ViewData["id"] = id;
+            return View(spells.spells[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            if (Request.HasFormContentType)
+            {
+                if (Request.Form.TryGetValue("spell", out StringValues spellValue))
+                {
+                    spells.spells.Add(new SpellModel() { Name = spellValue[0] });
+                }
+            }
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete()
+        {
+            if (Request.HasFormContentType)
+            {
+                if (Request.Form.TryGetValue("spellIndex", out StringValues spellValue))
+                {
+                    if (int.TryParse(spellValue[0], out int spellIndex))
+                    {
+                        spells.spells.RemoveAt(spellIndex);
+                    }
+                }
+            }
+            return RedirectToAction("index");
+        }
+    }
+}
