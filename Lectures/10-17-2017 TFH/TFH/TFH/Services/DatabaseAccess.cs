1bd79ce19b0e1554844b29547dfd10b9cc117560
Steven Yackel <spazard1@gmail.com>
2017-10-17 15:57:48 -0500

    Add 10-17 TFH

---
+++ Lectures/10-17-2017 TFH/TFH/TFH/Services/DatabaseAccess.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TFH.Services
+{
+    public class DatabaseAccess
+    {
+        RetryPolicy rp = new RetryPolicy(new DetectionStrategy(), 10);
+
+        public string Get(string id)
+        {
+            
+            var result = rp.ExecuteAction(() =>
+            {
+                var myRequest = WebRequest.Create("www.google.com");
+                var response = myRequest.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                return responseString;
+            });
+
+            return result;
+        }
+    }
+}
